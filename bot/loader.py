d666355d332c6ac7bae44017f61a622037100eef
vladdd0 <vlad123123v@gmail.com>
2023-05-20 00:18:49 +0300

    Refactor code

--- bot/loader.py
+++ bot/loader.py
@@ -1,15 +1,11 @@
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot
+from aiogram import Dispatcher
+from aiogram import types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from data import config
-from utils.db_SQLite.db import Database
 
-db = Database("/Users/pavelpopov/Documents/TraderBot/db.db")
-# db = Database('/root/bot/db.db')
 
-bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
-
-storage = MemoryStorage()
-
-dp = Dispatcher(bot, storage=storage)
-
-__all__ = ["bot", "storage", "dp", "db"]
+class Loader:
+    bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+    storage = MemoryStorage()
+    dp = Dispatcher(bot, storage=storage)
--- bot/middlewares/throttling.py
+++ bot/middlewares/throttling.py
@@ -80,3 +80,21 @@ class ThrottlingMiddleware(BaseMiddleware):
         # If current message is not last with current key - do not send message
         # if thr.exceeded_count == throttled.exceeded_count:
         #     await message.reply('Unlocked.')
+
+
+def rate_limit(limit: int, key=None):
+    """
+    Decorator for configuring rate limit and key in different functions.
+
+    :param limit:
+    :param key:
+    :return:
+    """
+
+    def decorator(func):
+        setattr(func, "throttling_rate_limit", limit)
+        if key:
+            setattr(func, "throttling_key", key)
+        return func
+
+    return decorator
